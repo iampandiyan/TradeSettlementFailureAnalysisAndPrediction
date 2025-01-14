781921cb71d4079b8eec0b4e87a0c1d66bea3927
iampandiyan <mepandiyan@gmail.com>
2025-01-14 12:04:29 +0530

    Trade Settlement Failure Analysis and Prediction

--- 3. Univariate and Bivariate/AnalysisUtils.py
+++ 3. Univariate and Bivariate/AnalysisUtils.py
@@ -17,24 +17,24 @@ class AnalysisUtils():
         quanColumns=self.quanQual(dataset)[0]
         descriptive=pd.DataFrame(index=["Mean","Median","Mode","Q1:25%","Q2:50%","Q3:75%","99%","Q4:100%","IQR","1.5rule","Lesser","Greater","Min","Max","Kurtosis","Skew","Var","Std"],columns=quanColumns)        
         for columnName in quanColumns:
-            descriptive[columnName]["Mean"]=dataset[columnName].mean()
-            descriptive[columnName]["Median"]=dataset[columnName].median()
-            descriptive[columnName]["Mode"]=dataset[columnName].mode()[0]
-            descriptive[columnName]["Q1:25%"]=dataset.describe()[columnName]["25%"]
-            descriptive[columnName]["Q2:50%"]=dataset.describe()[columnName]["50%"]
-            descriptive[columnName]["Q3:75%"]=dataset.describe()[columnName]["75%"]
-            descriptive[columnName]["99%"]=np.percentile(dataset[columnName],99)
-            descriptive[columnName]["Q4:100%"]=dataset.describe()[columnName]["max"]
-            descriptive[columnName]["IQR"]=descriptive[columnName]["Q3:75%"]-descriptive[columnName]["Q1:25%"]
-            descriptive[columnName]["1.5rule"]=1.5*descriptive[columnName]["IQR"]
-            descriptive[columnName]["Lesser"]=descriptive[columnName]["Q1:25%"]-descriptive[columnName]["1.5rule"]
-            descriptive[columnName]["Greater"]=descriptive[columnName]["Q3:75%"]+descriptive[columnName]["1.5rule"]
-            descriptive[columnName]["Min"]=dataset[columnName].min()
-            descriptive[columnName]["Max"]=dataset[columnName].max()
-            descriptive[columnName]["Kurtosis"]=dataset[columnName].kurtosis()
-            descriptive[columnName]["Skew"]=dataset[columnName].skew()
-            descriptive[columnName]["Var"]=dataset[columnName].var()
-            descriptive[columnName]["Std"]=dataset[columnName].std()
+            descriptive.loc["Mean",columnName]=dataset[columnName].mean()
+            descriptive.loc["Median",columnName]=dataset[columnName].median()
+            descriptive.loc["Mode",columnName]=dataset[columnName].mode()[0]
+            descriptive.loc["Q1:25%",columnName]=dataset.describe()[columnName]["25%"]
+            descriptive.loc["Q2:50%",columnName]=dataset.describe()[columnName]["50%"]
+            descriptive.loc["Q3:75%",columnName]=dataset.describe()[columnName]["75%"]
+            descriptive.loc["99",columnName]=np.percentile(dataset[columnName],99)
+            descriptive.loc["Q4:100%",columnName]=dataset.describe()[columnName]["max"]
+            descriptive.loc["IQR",columnName]=descriptive[columnName]["Q3:75%"]-descriptive[columnName]["Q1:25%"]
+            descriptive.loc["1.5rule",columnName]=1.5*descriptive[columnName]["IQR"]
+            descriptive.loc["Lesser",columnName]=descriptive[columnName]["Q1:25%"]-descriptive[columnName]["1.5rule"]
+            descriptive.loc["Greater",columnName]=descriptive[columnName]["Q3:75%"]+descriptive[columnName]["1.5rule"]
+            descriptive.loc["Min",columnName]=dataset[columnName].min()
+            descriptive.loc["Max",columnName]=dataset[columnName].max()
+            descriptive.loc["Kurtosis",columnName]=dataset[columnName].kurtosis()
+            descriptive.loc["Skew",columnName]=dataset[columnName].skew()
+            descriptive.loc["Var",columnName]=dataset[columnName].var()
+            descriptive.loc["Std",columnName]=dataset[columnName].std()
         return descriptive
     
     def getOutliers(self,dataset):
@@ -54,9 +54,9 @@ class AnalysisUtils():
         lesserCoulmNames=self.getOutliers(dataset)[0]
         greaterColumnNames=self.getOutliers(dataset)[1]
         for columnName in lesserCoulmNames:
-            dataset[columnName][dataset[columnName]<descriptive[columnName]["Lesser"]]=descriptive[columnName]["Lesser"]
+            dataset.loc[dataset[columnName] < descriptive[columnName]["Lesser"], columnName] = descriptive[columnName]["Lesser"]
         for columnName in greaterColumnNames:
-            dataset[columnName][dataset[columnName]>descriptive[columnName]["Greater"]]=descriptive[columnName]["Greater"]
+            dataset.loc[dataset[columnName] > descriptive[columnName]["Greater"], columnName] = descriptive[columnName]["Greater"]
 
     def createFrequencyTable(self,columnName,dataset):
         freqTable=pd.DataFrame(columns=["Unique_values","Frequency","Relative_Frequency","Cumsum"])
